9ea4e58ed692ef12f6a827d097e2fa164fcef8bf
Hankin <whq78164@gmail.com>
2024-10-28 18:14:26 +0800

    UPDATE updatessl

--- sslupdate.sh
+++ sslupdate.sh
@@ -4,14 +4,17 @@
 /usr/local/bin/docker-compose -f /root/docker/docker-compose.yml stop
 
 # 内建80端口的web服务器执行证书申请或续期
- runargs="--email=xxxx@qq.com --domains=a.abc.com --http --accept-tos --path=/root/docker/services/nginx/ssl/"
+# runargs1="--accept-tos --email=xxxx@qq.com --domains=abc.com,www.abc.com --http --path=/root/docker/services/nginx/ssl/ --http.webroot=/path/to/webroot"
+runargs1="--accept-tos --email=xxxx@qq.com --domains=abc.com,www.abc.com --http --path=/root/docker/services/nginx/ssl/"
+runargs2="--accept-tos --email=xxxx@qq.com --domains=b.abc.com --http --path=/root/docker/services/nginx/ssl/" 
 
 # OBTAIN A CERTIFICATE 首次运行时执行证书申请命令
-# /root/apps/lego ${runargs} run
+# /root/apps/lego ${runargs1} run
+# /root/apps/lego ${runargs2} run
 
 # RENEW A CERTIFICATE 申请证书后，把证书续期加入定时任务
- /root/apps/lego ${runargs} renew
- /root/apps/lego --email=xxxx@qq.com --domains=b.abc.com --http --accept-tos --path=/root/docker/services/nginx/sslenew
+ /root/apps/lego ${runargs1} renew
+ /root/apps/lego ${runargs1} renew
 
 # 重新启动docker
 /usr/local/bin/docker-compose -f /root/docker/docker-compose.yml up -d
@@ -22,3 +25,13 @@ docker exec -t nginx /bin/bash "/www/aftersslrenew.sh"
 
 # 每周日凌晨1点10分执行该脚本
 # echo "10 1 * * 0 /home/root/apps/lego.sh >> /home/root/apps/lego.output 2>&1" >>  /var/spool/cron/crontabs/root
+
+# server {
+#     listen       80;
+#     listen    443 ssl;
+#     ssl_certificate /ssl/certificates/abc.com.crt;
+#     ssl_certificate_key /ssl/certificates/abc.com.key;
+# }
+
+
+# https://go-acme.github.io/lego/usage/cli/renew-a-certificate/index.html
