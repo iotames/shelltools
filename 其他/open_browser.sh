b8c360ccc35c8e89c85d96d2356d7356bb3f7ed0
Hankin <whq78164@gmail.com>
2024-10-30 10:10:26 +0800

    ADD open_browser

---
+++ 其他/open_browser.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# FOR WINDOWS "C:\Program Files\Google\Chrome\Application\chrome.exe" --log-level=1 --window-size="1366,768" --user-data-dir="D:\chrome_user_data" --proxy-server=http://172.16.100.28:8889 --new-window https://www.baidu.com/
+
+/usr/bin/google-chrome-stable --log-level=1 --window-size="1366,768" --user-data-dir="/root/chrome_user_data" --proxy-server=http://172.16.100.28:8889 --new-window https://www.baidu.com/
